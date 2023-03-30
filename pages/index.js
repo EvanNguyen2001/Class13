b5d9f9df959bdde3ad1cf444285f1332b05aaf5c
EvanNguyen2001 <113669106+EvanNguyen2001@users.noreply.github.com>
2023-03-30 14:50:55 -0700

    initial

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,26 @@
 import Head from 'next/head'
-import Image from 'next/image'
 import styles from '@/styles/Home.module.css'
-import BarChart from '@/components/BarChart'
-
+import {useState, useEffect } from 'react'
+import { matrix } from "../data/matrix.js"
+import Accordian from "../components/Accordion"
 
 export default function Home() {
+
+  const [totalCredits, setTotalCredits] = useState(0);
+  
+  const [data, setData] = useState([...matrix.courses]);
+
+  var title = process.env.NEXT_PUBLIC_TITLE;
+
+  useEffect(() => {
+    var x = 0;
+
+    data.map((i) => {
+      x += i.credits
+    })
+      setTotalCredits(x);
+    }, [])
+
   return (
     <>
       <Head>
@@ -14,7 +30,19 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className={styles.main}>
-        <BarChart/>
+        {title}
+
+        {data && data.map ((info, index) => {
+          return(
+            <Accordian key ={index}
+            term={info.term}
+            code={info.code}
+            title={info.title}
+            credits={info.credits}
+            description={info.description}
+            />
+          )
+        })}
       </main>
     </>
   )
